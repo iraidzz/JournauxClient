63ce799661520dc90ebdae875d4bcdf5c869b3fd
iraidzz <iraidzz.13@gmail.com>
2017-06-30 13:50:39 +0200

    Fin modif client depuis client vers API : OK

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -199,57 +199,69 @@ class ClientController extends Controller
         return view('editclient')->with('compteinfo', $compteinfo);
     }
 
-    public function EditCompte()
-    {
+//    public function EditCompte()
+//    {
+//
 //        $ch = curl_init();
-        // configuration des options
-//        curl_setopt($ch, CURLOPT_URL, "http://journaux.dev/api/client/enregistrer");
+//        // configuration des options
 //        curl_setopt($ch, CURLOPT_URL, "http://journaux.dev/api/client/edit");
-
-        $url = "http://journaux.dev/api/client/edit";
-
-        $options = array(
-            CURLOPT_RETURNTRANSFER => true,   // return web page
-            CURLOPT_HEADER         => false,  // don't return headers
-            CURLOPT_FOLLOWLOCATION => true,   // follow redirects
-            CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
-            CURLOPT_ENCODING       => "",     // handle compressed
-            CURLOPT_USERAGENT      => "", // name of client
-            CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
-            CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
-            CURLOPT_TIMEOUT        => 120,    // time-out on response
-        );
-
-        $ch = curl_init($url);
-        curl_setopt_array($ch, $options);
-
 //        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 //        curl_setopt($ch, CURLOPT_HEADER, 0);
-        // data à envoyer
-        $data = [];
-        $data['user']['name'] = request()->only('name')['name'];
-        $data['user']['email'] = request()->only('email')['email'];
-        $data['user']['password'] = request()->only('password')['password'];
-        $data['user']['prenom'] = request()->only('prenom')['prenom'];
-        $data['user']['civilite'] = request()->only('civilite')['civilite'];
-        $data['user']['numero_telephone'] = request()->only('numero_telephone')['numero_telephone'];
-        $data['user']['date_naissance'] = request()->only('date_naissance')['date_naissance'];
-        $data['user']['lieu_naissance'] = request()->only('lieu_naissance')['lieu_naissance'];
-        $data['user']['adresse_domicile'] = request()->only('adresse_domicile')['adresse_domicile'];
-        $data['user']['postal_domicile'] = request()->only('postal_domicile')['postal_domicile'];
-        $data['user']['ville_domicile'] = request()->only('ville_domicile')['ville_domicile'];
+//
+//
+//        // data à envoyer
+//        $data = [];
+//        $data['user']['name'] = request()->only('name')['name'];
+//        $data['user']['email'] = request()->only('email')['email'];
+//        $data['user']['password'] = request()->only('password')['password'];
+//        $data['user']['prenom'] = request()->only('prenom')['prenom'];
+//        $data['user']['civilite'] = request()->only('civilite')['civilite'];
+//        $data['user']['numero_telephone'] = request()->only('numero_telephone')['numero_telephone'];
+//        $data['user']['date_naissance'] = request()->only('date_naissance')['date_naissance'];
+//        $data['user']['lieu_naissance'] = request()->only('lieu_naissance')['lieu_naissance'];
+//        $data['user']['adresse_domicile'] = request()->only('adresse_domicile')['adresse_domicile'];
+//        $data['user']['postal_domicile'] = request()->only('postal_domicile')['postal_domicile'];
+//        $data['user']['ville_domicile'] = request()->only('ville_domicile')['ville_domicile'];
+//
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+//
+//        // exécution de la session : $compteinfo est le retour de l'api
+//        $compteinfo = json_decode(curl_exec($ch), true);
+//
+//        dd($ch);
+//        // fermeture de la session
+//        curl_close($ch);
+//
+//        return redirect('/intro');
+//    }
 
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    public function EditCompte()
+    {
+        $id=request()->only('id')['id'];
+        $name=request()->only('name')['name'];
+        $email=request()->only('email')['email'];
+        $password=request()->only('password')['password'];
+        $prenom=request()->only('prenom')['prenom'];
+        $civilite=request()->only('civilite')['civilite'];
+        $numero_telephone=request()->only('numero_telephone')['numero_telephone'];
+        $date_naissance=request()->only('date_naissance')['date_naissance'];
+        $lieu_naissance=request()->only('lieu_naissance')['lieu_naissance'];
+        $adresse_domicile=request()->only('adresse_domicile')['adresse_domicile'];
+        $postal_domicile=request()->only('postal_domicile')['postal_domicile'];
+        $ville_domicile=request()->only('ville_domicile')['ville_domicile'];
 
-        // exécution de la session : $compteinfo est le retour de l'api
-        $compteinfo = json_decode(curl_exec($ch), true)['result'];
+        $ch = curl_init();
+        $url = "http://journaux.dev/api/client/edit";
+        // configuration des options
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS,"id=$id&name=$name&email=$email&password=$password&prenom=$prenom&civilite=$civilite&numero_telephone=$numero_telephone&date_naissance=$date_naissance&lieu_naissance=$lieu_naissance&adresse_domicile=$adresse_domicile&postal_domicile=$postal_domicile&ville_domicile=$ville_domicile&");
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+        $content =curl_exec($ch);
 
-        // fermeture de la session
         curl_close($ch);
 
-        dd($compteinfo);
-
-        return view('editclient')->with('compteinfo', $compteinfo);
+                return redirect('/intro');
 
     }
 
